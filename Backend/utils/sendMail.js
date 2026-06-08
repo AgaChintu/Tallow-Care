beca3dba5fed5b1d1680436fc1d82fed3be8545c
SOMYA AGARWAL <agarwalsomya796@gmail.com>
2026-06-08 13:13:28 +0530

    Import fetch from node-fetch in sendMail.js

--- Backend/utils/sendMail.js
+++ Backend/utils/sendMail.js
@@ -1,3 +1,4 @@
+import fetch from "node-fetch";
 export const sendOTPEmail = async (toEmail, otp, userName = "there") => {
   try {
     const response = await fetch("https://api.brevo.com/v3/smtp/email", {
